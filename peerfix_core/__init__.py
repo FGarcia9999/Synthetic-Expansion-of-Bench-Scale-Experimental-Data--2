1f96d588f12ef0e276f0e2eeea78e17b9f9f06f1
Fernando Garcia <fernandogarcia9999@gmail.com>
2026-09-13 12:44:39 -0300

    Gate 0.4: expose preflight sensitivity and fidelity APIs

--- peerfix_core/__init__.py
+++ peerfix_core/__init__.py
@@ -7,6 +7,21 @@ from .icd import evaluate_icd_matched_n
 from .dcr import compute_dcr
 from .generators import GeneratorSpec, available_generators, build_generator, seed_everything
 from .tabddpm import SmallNTabDDPM, TabDDPMConfig
+from .sensitivity import (
+    ResponseJitterCalibration,
+    calibrate_response_jitter,
+    sensitivity_noise_seed,
+    apply_response_jitter,
+    wrap_generator_with_response_jitter,
+)
+from .protocol import combined_protocol_sha256, validate_gate04_preflight
+from .fidelity import (
+    continuous_univariate_fidelity,
+    designed_support_fidelity,
+    correlation_fidelity,
+    doe_structure_fidelity,
+    evaluate_fidelity_bundle,
+)
 
 __all__ = [
     "derive_seed",
@@ -16,6 +31,11 @@ __all__ = [
     "compute_dcr",
     "GeneratorSpec", "available_generators", "build_generator", "seed_everything",
     "SmallNTabDDPM", "TabDDPMConfig",
+    "ResponseJitterCalibration", "calibrate_response_jitter", "sensitivity_noise_seed",
+    "apply_response_jitter", "wrap_generator_with_response_jitter",
+    "combined_protocol_sha256", "validate_gate04_preflight",
+    "continuous_univariate_fidelity", "designed_support_fidelity",
+    "correlation_fidelity", "doe_structure_fidelity", "evaluate_fidelity_bundle",
 ]
 
-__version__ = "1.0.0-pre.1"
+__version__ = "1.0.0-pre.2"
